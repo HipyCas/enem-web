81bb0e9bd358f7f6936b81896beeaa4241269ed8
Hipy Cas <hipycas@gmail.com>
2024-12-15 18:56:53 +0100

    dev: improve deploy script interface
    Improves the interface for the deploy script by showing the progress
    instead of the verbose log, allowing still to show the verbose log if -v
    or --verbose is indicated, showing the status with nice colors and
    showing a text over the whole status indicating where it is being
    deployed to (dimmed if test environment and yellow background if prod
    environment).

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,8 @@
 import { Client } from "basic-ftp";
 import "dotenv/config";
 import chalk from "chalk";
+import hr from '@tsmx/human-readable'
 
-import path from "node:path";
 import { argv } from "node:process";
 
 const prod = argv.includes("prod");
@@ -20,7 +20,15 @@ if (
   process.exit(1);
 
 const client = new Client();
-client.ftp.verbose = true;
+client.ftp.verbose = argv.includes('-') || argv.includes('--verbose');
+
+const showProgress: Parameters<typeof client["trackProgress"]>[0] = (info) => {
+  console.log("\x1b[2J")
+  console.log(chalk.dim(prod ? chalk.bgYellow("⚠ Uploading to production") : "🛈 Uploading to prueba.anem.es/2025"))
+  console.log(chalk.bold(`${chalk.green(capitalizeFirstLetter(info.type))} ${chalk.blue(info.name)}`));
+  console.log("Transferred:", humanBytes(info.bytes))
+  console.log("Transferred Overall:", humanBytes(info.bytesOverall))
+}
 
 async function main() {
   try {
@@ -36,6 +44,7 @@ async function main() {
     });
     // console.info(await client.list(""));
 
+    client.trackProgress(showProgress);
     await client.uploadFromDir(process.env.DEPLOY_FTP_FOLDER as string, "/");
     // console.info(client.list(""));
   } catch (error) {
@@ -46,3 +55,11 @@ async function main() {
 }
 
 main();
+
+function humanBytes(bytes: number) {
+  return hr.fromBytes(bytes, { fixedPrecision: 2 });
+}
+
+function capitalizeFirstLetter(val) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
