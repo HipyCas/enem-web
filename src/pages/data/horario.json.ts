92b9ada7b0fd7c0e3babf37f691623981ef0925d
Hipy Cas <hipycas@gmail.com>
2025-01-17 17:27:11 +0100

    page: Horario
    Página de horario. También pone disponible el horario como JSON en
    /data/horario.json.

---
+++ src/pages/data/horario.json.ts
@@ -0,0 +1,546 @@
+export const HORARIO = {
+  "22": [
+    {
+      name: "Check-in",
+      description:
+        "Check-in al evento. Se te entregará la acreditación y el pack de bienvenida. **Esto no es el check-in al alojamiento**",
+      location: "Hall Facultad de Ciencias",
+      start: "12:00",
+      end: "17:30",
+      category: "check-in",
+    },
+    {
+      name: "Presentación",
+      description:
+        "La organización del XXVI ENEM lo presentará y explicará todos los apartados de él, así como una introducción a los próximos días.",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "organizacion",
+      start: "18:30",
+      end: "19:30",
+    },
+    {
+      name: "Conferencia Inaugural",
+      description: "",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "19:30",
+      end: "20:30",
+    },
+    {
+      name: "Acto Inaugural",
+      description: "",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "actos_institucionales",
+      start: "20:30",
+      end: "21:00",
+    },
+    {
+      name: "Cóctel Inaugural",
+      description:
+        "Para abrir el evento, tendremos un cóctel inaugural junto a la sala principal antes de irnos a los alojamientos.",
+      location: "Cafetería de la Facultad de Ciencias",
+      category: "cenas_indicadas",
+      start: "21:00",
+      end: "22:30",
+    },
+    {
+      name: "Actividades culturales y sociales",
+      description:
+        "Diferentes actividades culturales y sociales para conocernos y descubrir Granada",
+      location: "Algún sitio en Granada",
+      category: "actividades_culturales",
+      start: "22:30",
+    },
+  ],
+  "23": [
+    {
+      name: "Desayuno",
+      description:
+        "Desayuno en el alojamiento correspondiente, siendo en cualquier momento en el plazo indicado. **Incluye traslado a la Facultad**.",
+      location: "Alojamiento correspondiente",
+      category: "desayunos",
+      start: "8:00",
+      end: "9:00",
+    },
+    {
+      name: "Energizer",
+      description: "Introducción del día y breves dinámicas.",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "energizer",
+      start: "9:00",
+      end: "9:30",
+    },
+    {
+      name: "Conferencia 1",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "9:30",
+      end: "10:30",
+    },
+    {
+      name: "Conferencia 2",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "10:30",
+      end: "11:30",
+    },
+    {
+      name: "Coffee Break",
+      description:
+        "Una breve pausa para tomar un café y unos aperitivos antes de seguir.",
+      location: "Hall Facultad de Ciencias",
+      category: "coffee_break",
+      start: "11:30",
+      end: "12:00",
+    },
+    {
+      name: "Conferencia 3",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "12:00",
+      end: "13:00",
+    },
+    {
+      name: "Conferencia 4",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "13:00",
+      end: "14:00",
+    },
+    {
+      name: "Comida",
+      description: "",
+      location: "Cafeteria de la Facultad de Ciencias",
+      category: "Comedor Universitario",
+      start: "14:00",
+      end: "15:30",
+    },
+    {
+      name: "Charla ANEM",
+      description:
+        "Presentamos la ANEM, la Asociación Nacional de Estudiantes de Matemáticas, y la encargada de dar vida a este evento.",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "charlas_varias",
+      start: "16:00",
+      end: "17:00",
+    },
+    {
+      name: "Mesa Redonda 1",
+      description: "_Mesa Redonda por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "mesas_redondas",
+      start: "17:00",
+      end: "18:00",
+    },
+    {
+      name: "Coffee Break",
+      description:
+        "Una breve pausa para tomar un café y unos aperitivos antes de seguir.",
+      location: "Hall Facultad de Ciencias",
+      category: "coffee_break",
+      start: "18:00",
+      end: "18:30",
+    },
+    {
+      name: "Conferencia 5",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "18:30",
+      end: "19:30",
+    },
+    {
+      name: "Conferencia 6",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "19:30",
+      end: "20:30",
+    },
+    {
+      name: "Cena",
+      description: "_Cenas por determinar_",
+      location: "Por determinar",
+      category: "cenas",
+      start: "21:00",
+      end: "22:30",
+    },
+    {
+      name: "Actividades culturales y sociales",
+      description:
+        "Diferentes actividades culturales y sociales para conocernos y descubrir Granada",
+      location: "Algún sitio en Granada",
+      category: "actividades_culturales",
+      start: "22:30",
+    },
+  ],
+  "24": [
+    {
+      name: "Desayuno",
+      description:
+        "Desayuno en el alojamiento correspondiente, siendo en cualquier momento en el plazo indicado. **Incluye traslado a la Facultad**.",
+      location: "Alojamiento correspondiente",
+      category: "desayuno",
+      start: "8:00",
+      end: "9:00",
+    },
+    {
+      name: "Pechakuchas",
+      description:
+        "Algunas de las entidades colaboradoras se presentarán a todos los asistentes en un par de minutos.",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "entidades",
+      start: "9:00",
+      end: "9:30",
+    },
+    {
+      name: "Foro de Empresas y Emprendimiento",
+      description:
+        "Conoce a empresas y entidades del ámbito matemático y participa en sorteos y entrevistas.",
+      location: "Hall Facultad de Ciencias",
+      category: "foro",
+      start: "9:30",
+      end: "11:30",
+    },
+    {
+      name: "Talleres",
+      description:
+        "Disfruta y aprende con talleres de grupos reducidos de diversos temas de interés.",
+      location: "Salas varias",
+      category: "talleres",
+      start: "9:30",
+      end: "11:30",
+    },
+    {
+      name: "Microcharlas",
+      description:
+        "Aprende de temas concretos y muy variados en las microcharlas de corta duración, dinámicas y completas.",
+      location: "Salas varias",
+      category: "microcharlas",
+      start: "9:30",
+      end: "11:30",
+    },
+    {
+      name: "Coffee Break",
+      description:
+        "Una breve pausa para tomar un café y unos aperitivos antes de seguir.",
+      location: "Hall Facultad de Ciencias",
+      category: "coffee_break",
+      start: "11:30",
+      end: "12:00",
+    },
+    {
+      name: "Foro de Empresas y Emprendimiento",
+      description:
+        "Conoce a empresas y entidades del ámbito matemático y participa en sorteos y entrevistas.",
+      location: "Hall Facultad de Ciencias",
+      category: "foro",
+      start: "12:00",
+      end: "14:00",
+    },
+    {
+      name: "Talleres",
+      description:
+        "Disfruta y aprende con talleres de grupos reducidos de diversos temas de interés.",
+      location: "Salas varias",
+      category: "talleres",
+      start: "12:00",
+      end: "14:00",
+    },
+    {
+      name: "Microcharlas",
+      description:
+        "Aprende de temas concretos y muy variados en las microcharlas de corta duración, dinámicas y completas.",
+      location: "Salas varias",
+      category: "microcharlas",
+      start: "12:00",
+      end: "14:00",
+    },
+    {
+      name: "Comida",
+      description: "",
+      location: "Comedor Universitario",
+      category: "comidas",
+      start: "14:00",
+      end: "15:30",
+    },
+    {
+      name: "Foro de Empresas y Emprendimiento",
+      description:
+        "Conoce a empresas y entidades del ámbito matemático y participa en sorteos y entrevistas.",
+      location: "Hall Facultad de Ciencias",
+      category: "foro",
+      start: "16:00",
+      end: "18:00",
+    },
+    {
+      name: "Talleres",
+      description:
+        "Disfruta y aprende con talleres de grupos reducidos de diversos temas de interés.",
+      location: "Salas varias",
+      category: "talleres",
+      start: "16:00",
+      end: "18:00",
+    },
+    {
+      name: "Microcharlas",
+      description:
+        "Aprende de temas concretos y muy variados en las microcharlas de corta duración, dinámicas y completas.",
+      location: "Salas varias",
+      category: "microcharlas",
+      start: "16:00",
+      end: "18:00",
+    },
+    {
+      name: "Entrevistas",
+      description:
+        "Atiende a entrevistas organizadas por empresas colaboradores a las que te podrás haber inscrito durante el Foro de Empresas y Emprendimiento.",
+      location: "Salas varias",
+      category: "entrevistas",
+      start: "16:00",
+      end: "18:00",
+    },
+    {
+      name: "Coffee Break",
+      description:
+        "Una breve pausa para tomar un café y unos aperitivos antes de seguir.",
+      location: "Hall Facultad de Ciencias",
+      category: "coffee_break",
+      start: "18:00",
+      end: "18:30",
+    },
+    {
+      name: "Conferencia 7",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "18:30",
+      end: "19:30",
+    },
+    {
+      name: "Conferencia 8",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "19:30",
+      end: "20:30",
+    },
+    {
+      name: "Entrevistas",
+      description:
+        "Atiende a entrevistas organizadas por empresas colaboradores a las que te podrás haber inscrito durante el Foro de Empresas y Emprendimiento.",
+      location: "Salas varias",
+      category: "entrevistas",
+      start: "18:30",
+      end: "20:30",
+    },
+    {
+      name: "Cena",
+      description: "_Cenas por determinar_",
+      location: "Por determinar",
+      category: "cenas",
+      start: "21:00",
+      end: "22:30",
+    },
+    {
+      name: "Actividades culturales y sociales",
+      description:
+        "Diferentes actividades culturales y sociales para conocernos y descubrir Granada",
+      location: "Algún sitio en Granada",
+      category: "actividades_culturales",
+      start: "22:30",
+    },
+  ],
+  "25": [
+    {
+      name: "Desayuno",
+      description:
+        "Desayuno en el alojamiento correspondiente, siendo en cualquier momento en el plazo indicado. **Incluye traslado a la Facultad**.",
+      location: "Alojamiento correspondiente",
+      category: "desayuno",
+      start: "8:00",
+      end: "9:00",
+    },
+    {
+      name: "Energizer",
+      description: "Introducción del día y breves dinámicas.",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "energizer",
+      start: "9:00",
+      end: "9:30",
+    },
+    {
+      name: "Conferencia 9",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "9:30",
+      end: "10:30",
+    },
+    {
+      name: "Conferencia 10",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "10:30",
+      end: "11:30",
+    },
+    {
+      name: "Coffee Break",
+      description:
+        "Una breve pausa para tomar un café y unos aperitivos antes de seguir.",
+      location: "Hall Facultad de Ciencias",
+      category: "coffee_break",
+      start: "11:30",
+      end: "12:00",
+    },
+    {
+      name: "Actividad por determinar",
+      description: "_Actividad por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "charlas_varias",
+      start: "12:00",
+      end: "12:30",
+    },
+    {
+      name: "Mesa Redonda 2",
+      description: "_Mesa Redonda por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "mesas_redondas",
+      start: "12:30",
+      end: "14:00",
+    },
+    {
+      name: "Comida",
+      description: "",
+      location: "Comedor Universitario",
+      category: "comidas",
+      start: "14:00",
+      end: "15:30",
+    },
+    {
+      name: "Conferencia 11",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "16:00",
+      end: "17:00",
+    },
+    {
+      name: "Conferencia 12",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "17:00",
+      end: "18:00",
+    },
+    {
+      name: "Coffee Break",
+      description:
+        "Una breve pausa para tomar un Café y unos aperitivos antes de seguir.",
+      location: "Hall Facultad de Ciencias",
+      category: "coffee_break",
+      start: "18:00",
+      end: "18:30",
+    },
+    {
+      name: "Conferencia de Clausura",
+      description: "_Conferencia por determinar_",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "conferencias",
+      start: "18:30",
+      end: "19:30",
+    },
+    {
+      name: "Acto de Clausura",
+      description:
+        "Despedimos este evento con una pequeña intervención de la organización, agradeciendo la participación de todos.",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "organizacion",
+      start: "19:30",
+      end: "20:00",
+    },
+    {
+      name: "Mesa de Clausura",
+      description:
+        "Cerramos finalmente el evento con una última mesa institucional de despedidad",
+      location: "Sala ENEM GRX (Aula Magna)",
+      category: "actos_institucionales",
+      start: "20:00",
+      end: "20:30",
+    },
+    {
+      name: "Cena",
+      description: "_Cenas por determinar_",
+      location: "Por determinar",
+      category: "cenas",
+      start: "21:00",
+      end: "22:30",
+    },
+    {
+      name: "Actividades culturales y sociales",
+      description:
+        "Diferentes actividades culturales y sociales para conocernos y descubrir Granada",
+      location: "Algún sitio en Granada",
+      category: "actividades_culturales",
+      start: "22:30",
+    },
+  ],
+  "26": [
+    {
+      name: "Desayuno",
+      description:
+        "Desayuno en el alojamiento correspondiente, siendo en cualquier momento en el plazo indicado. **Incluye traslado a las actividades correspondientes**.",
+      location: "Alojamiento correspondiente",
+      category: "desayuno",
+      start: "8:00",
+      end: "9:00",
+    },
+    {
+      name: "Turismo y actividades culturales",
+      description:
+        "Desde la organización se organizará múltiples actividades culturales dentro del Pase Cultural para los asistentes que las deseen. Además, los asistentes podrán visitar a su gusto la ciudad independientemente.",
+      location: "Granada",
+      category: "actividades_culturales",
+      start: "9:00",
+      end: "20:30",
+    },
+    {
+      name: "Cena de Clausura",
+      description:
+        "Una cena de gala para clausurar el evento y despedirnos todos disfrutando en conjunto.",
+      location: "Por determinar",
+      category: "cenas_indicadas",
+      start: "21:00",
+      end: "23:00",
+    },
+  ],
+  "27": [
+    {
+      name: "Desayuno",
+      description:
+        "Desayuno en el alojamiento correspondiente, siendo en cualquier momento en el plazo indicado. **Incluye traslado a las actividades correspondientes**.",
+      location: "Alojamiento correspondiente",
+      category: "desayuno",
+      start: "8:00",
+      end: "9:00",
+    },
+    {
+      name: "Salida",
+      description:
+        "Fin definitivo del evento y salida de los asistentes de los alojamientos.",
+      location: "Alojamientos",
+      category: "salida",
+      start: "9:00",
+      end: "12:30",
+    },
+  ],
+};
+
+export async function GET() {
+  return new Response(JSON.stringify(HORARIO));
+}
