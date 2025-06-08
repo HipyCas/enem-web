81d30a0678ddc18bdf61e01b5834b814268e97cb
Dagongamer <davidgonzalezleon21@gmail.com>
2025-06-08 17:49:51 +0200

    Updated schedule

--- src/pages/data/horario.json.ts
+++ src/pages/data/horario.json.ts
@@ -19,8 +19,8 @@ export const HORARIO = {
       end: "19:30",
     },
     {
-      name: "Conferencia Inaugural",
-      description: "Conferencia dada por _Clara Grima_.",
+      name: "Conferencia Inaugural: Cómo enseñarle intuición a tu ordenador",
+      description: "**Ponente:**  Clara Grima (Coria del Río, 1971) Es doctora en matemáticas y profesora titular de Matemática Aplicada en la Universidad de Sevilla. Su investigación se enmarca en la Geometría Computacional y la Teoría de Grafos. De sus publicaciones se siente especialmente orgullosa de su tesis doctoral publicada íntegramente con el título Computational Geometry on Surfaces por Springer y de su participación, junto a un grupo de investigadores, en la identificación de una nueva forma geométrica que se ha hallado en las células epiteliales de los tejidos curvos, los escutoides.  Desde 2010 se dedica, además, a la divulgación de las matemáticas en diferentes medios y programas de radio y televisión. Sus libros de divulgación han sido traducidos al francés, italiano, checo, húngaro, coreano, chino, etc. Ha sido la segunda mujer española (de la historia) invitada al Congreso Internacional de Matemáticas en 2022, fue uno de los 12 matemáticos de todo el mundo elegidos por la UNESCO para celebrar el primer Día Mundial de las Matemáticas en 2020 y se le concedió una medalla de la RSME en 2024. Ha recibido otras distinciones como ser la primera mujer en dar el Pregón de la Feria del Libro de Granada (2016), nombrada coriana del año (su pueblo natal) en 2018, es medalla de oro de la ciudad de Sevilla (2021) y medalla de oro de la provincia de Sevilla (2022). Fue incluida en la lista Forbes de las 22 personas más influyentes en el año 2022 y Casio la eligió como una de las científicas españolas que aparecen en sus calculadoras. Imparte cientos de charlas al año y está convencida de que a todo el mundo le gustan las matemáticas, aunque algunos todavía no lo sepan. **Descripción:** En esta charla vamos a describir la filosofía que subyace en los algoritmos de la Geometría Computacional, una rama de las matemáticas que vive entre las matemáticas y la computación.",
       location: "Sala ENEM GRX (Aula Magna)",
       category: "conferencias",
       start: "19:30",
@@ -79,8 +79,8 @@ export const HORARIO = {
       end: "10:30",
     },
     {
-      name: "Conferencia 2",
-      description: "Conferencia dada por _CASIO_.",
+      name: "Conferencia: ¿Que hace una matemática como tu en una IA como esta?",
+      description: "**Ponente:** Anabel Forte Deltell es licenciada en Matemáticas (2005) y en Ciencias y Técnicas Estadísticas (2006) por la Universitat de València, donde también obtuvo su doctorado en Matemáticas en 2011. Tras un período posdoctoral en la Universitat Jaume I de Castellón, regresó a la Universitat de València, donde actualmente es profesora titular. Su investigación se centra en técnicas estadísticas para la selección de modelos, área en la que ha colaborado con grupos de Europa, Estados Unidos y Australia, publicando más de 40 trabajos en libros y revistas científicas. Actualmente, dirige el Máster en Bioestadística de la Universitat de València y es guionista y presentadora de las Píldoras para comprender y utilizar la Información de la Sociedad Española de Estadística e Investigación Operativa. En el ámbito de la divulgación, coordina el proyecto StatWars, participa en programas de radio como Raíz de 5 en Radio 5, y escribe en su blog BayesAna: Estadística (casi) por todas partes, galardonado como mejor blog de ciencia en los Premios 20Blogs 2021. Es también autora del libro ¿Cómo sobrevivir a la incertidumbre? (NextDoor Publishers, 2022). **Descripción:** Vivimos en un mundo en el que el término Inteligencia Artificial aparece por todas partes, como un reto, como una oportunidad, como una amenaza, pero ¿qué pintamos quienes nos dedicamos a las matemáticas en este campo? ¿Es una nueva oportunidad laboral? ¿Es un campo en el que investigar? En esta charla exploraremos cómo las matemáticas juegan un papel fundamental en el desarrollo y funcionamiento de la Inteligencia Artificial (IA). Desde la estadística y el álgebra lineal hasta el cálculo y las teorías de la optimización, las herramientas matemáticas son esenciales para entrenar modelos de IA, procesar datos y tomar decisiones inteligentes. Veremos cómo los conceptos matemáticos que estudiamos en el grado, como las matrices, los algoritmos y la probabilidad, son la base de los sistemas de IA que usamos hoy en día y, con ello, trataremos de entender mejor las oportunidades que ofrecen las matemáticas para avanzar en este campo y cómo en ellas está la clave para hacer que la IA sea más eficiente, ética y comprensible.",
       location: "Sala ENEM GRX (Aula Magna)",
       category: "conferencias",
       start: "10:30",
@@ -104,8 +104,8 @@ export const HORARIO = {
       end: "13:00",
     },
     {
-      name: "Conferencia 4",
-      description: "_Conferencia por determinar_",
+      name: "Conferencia: Buscamos matemáticas",
+      description: "Conferencia dada por _CASIO_: Eloy Bernal Arellano. **Descripción:** M de Matemáticas. M de Maryam, de Margaret, de Marta, de María Josefa... de Muchas Matemáticas Maravillosas. Las que estuvieron, las que están y las que venís. Miremos a todas las Matemáticas del Mundo!",
       location: "Sala ENEM GRX (Aula Magna)",
       category: "conferencias",
       start: "13:00",
@@ -432,7 +432,7 @@ export const HORARIO = {
     },
     {
       name: "Conferencia: Códigos detectores y correctores de errores",
-      description: "Conferencia dada por _Luis Merino_: ¿Sirve para algo el Álgebra en el día a día ? Una de las aplicaciones más importantes, además de la criptografía, es la de los  códigos detectores y correctores. Sin ellos no funcionarían, por ejemplo, nuestros móviles u ordenadores porque no habría memorias eficientes para ellos. Éstos códigos están basados Álgebra lineal sobre cuerpos finitos y usan también anillos de polinomios y son fundamentales en todo tipo de intercambios de información.",
+      description: "**Ponente:** Luis Merino González es doctor en Matemáticas y profesor del Departamento de Álgebra de la Universidad de Granada. Actualmente es coordinador del máster inter-universitario Matemáticas y anteriormente ha sido Coordinador de los estudios de Matemáticas, Secretario de la Facultad de Ciencias y Director del Departamento de Álgebra. Colabora como profesor en el proyecto Estalmat-Andalucía para el estimulo del talento precoz en Matemáticas desde su fundación. Su recorrido docente abarca distintas asignaturas de Álgebra en los estudios de Matemáticas, Estadística, Ingeniería de Caminos, Ingeniería Informática e Ingeniería de Telecomunicación. Es coautor del libro \"Álgebra lineal con métodos elementales\". Tras una larga etapa investigando en teoría de anillos no conmutativos, en la actualidad lo hace en temas de fundamentos algebraicos de la Matemática difusa. **Descripción:** ¿Sirve para algo el Álgebra en el día a día ? Una de las aplicaciones más importantes, además de la criptografía, es la de los  códigos detectores y correctores. Sin ellos no funcionarían, por ejemplo, nuestros móviles u ordenadores porque no habría memorias eficientes para ellos. Éstos códigos están basados Álgebra lineal sobre cuerpos finitos y usan también anillos de polinomios y son fundamentales en todo tipo de intercambios de información.",
       location: "Sala ENEM GRX (Aula Magna)",
       category: "conferencias",
       start: "17:00",
@@ -449,7 +449,7 @@ export const HORARIO = {
     },
     {
       name: "Conferencia de Clausura",
-      description: "Conferencia dada por _Bert Jansen_.",
+      description: "**Ponente:** Bert Janssen es Profesor Titular en el Dpto de Física Teórica y del Cosmos de la Universidad de Granada. Originario de Bélgica, obtuvo la Licenciatura en Física en la Universidad de Lovaina e hizo el Doctorado en la Universidad de Groningen (Países Bajos). Estuvo de estancias postdoctorales en el Instituto de Física Teórica del UAM/CSIC en Madrid, en la Universidad de Durham (Reino Unido) y en la Universidad de Lovaina (Bélgica). Llegó a Granada en el 2004 con un contrato Ramón y Cajal, donde luego se estabilizó como Profesor Titular (2011). Sus temas de investigación incluyen teoría de cuerdas y supergravedad, geometría diferencial y extensiones de la Relatividad General. Lleva varios años impartiendo la asignatura de Relatividad General en la Licenciatura y el Grado de Física de la UGR, En el 2022 publicó un libro de texto Gravitación y Geometría por Editorial de la UGR. Imparte con regularidad charlas divulgativas de física a estudiantes de bachillerato, en el aula de mayores y a la comunidad universitaria.",
       location: "Sala ENEM GRX (Aula Magna)",
       category: "conferencias",
       start: "18:30",
